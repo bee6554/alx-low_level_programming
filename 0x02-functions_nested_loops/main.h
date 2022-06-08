8a69ffde6bea84762fe7e5a71bc121f34a0faa8a
beimnet <beimnet6554@gmail.com>
2022-06-08 00:50:14 -0700

    edited main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,17 @@
-#include "main.h"                                                       
+#ifndef _main.h_
+#define _main.h_
 
-                                                                           void print_alphabet(void)                                                  
-{                                                                          
-        int i;                                                             
-                                 
-        for (i = 97; i <= 122; i++)                                        
-        {                                                                  
-                _putchar(i);                                               
-        }                                                                  
-        _putchar('\n');                                                    
-}
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+
+
+#endif
