9f06d07fc200144bed8d6df8e7af827275afa85b
beimnet <beimnet6554@gmail.com>
2022-06-09 14:43:09 -0700

    added main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,33 @@
-#include "main.h""
+#include "main.h"
 
-void print_alphabet(void)
-{
-	int i;
+/**
+ * main - prints _putchar                                         
+ * rn: returns 0 for success                                                                             
+ *
+ *    */                                                                                                          
 
-	for (i = 97; i <= 122; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
-}
+                                                                                                             
+
+int main(void)                                                                                               
+
+{                                                                                                            
+
+	        _putchar('_');                                                                                       
+
+		        _putchar('p');                                                                                       
+
+			        _putchar('u');                                                                                       
+
+				        _putchar('t');                                                                                       
+
+					        _putchar('c');                                                                                       
+
+						        _putchar('h');                                                                                       
+
+							        _putchar('a');                                                                                       
+
+								        _putchar('r');                                                                                       
+
+									        _putchar('\n');                                                                                      
+
+										        return (0);    
