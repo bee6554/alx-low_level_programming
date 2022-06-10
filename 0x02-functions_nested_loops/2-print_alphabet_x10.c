35d6e6a98da4a9488b886ecd67ca801ebf137f24
beimnet <beimnet6554@gmail.com>
2022-06-10 01:47:01 -0700

    betty fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,12 +2,12 @@
 
 /**
  * print_alphabet_x10 - Prints the alphabet 10 times.
- * 
+ *
  */
 void print_alphabet_x10(void)
 {
 	int a, b;
-	
+
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 97; b <= 122; b++)
