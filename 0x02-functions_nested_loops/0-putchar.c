75543a0bb3e779173d4384bcfa4e2040cf9f659e
beimnet <beimnet6554@gmail.com>
2022-06-10 00:55:50 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
-/**                                                                         * main - prints _putchar     
- * Return: returns 0 for success   
+/**
+ * Return: returns 0 for success
  */
 
 int main(void)
 {
 	 _putchar('_');
-	 _putchar('p'); 
-	 _putchar('u'); 
-	 _putchar('t'); 
-	 _putchar('c'); 
-	 _putchar('h'); 
-	 _putchar('a'); 
-	 _putchar('r'); 
-	 _putchar('\n');
-    
- 	 return (0);
+ 	 _putchar('p'); 
+ 	 _putchar('u'); 
+ 	 _putchar('t'); 
+ 	 _putchar('c'); 
+ 	 _putchar('h'); 
+ 	 _putchar('a'); 
+ 	 _putchar('r'); 
+ 	 _putchar('\n');
+					     
+  	 return (0);
 }
